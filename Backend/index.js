8bc044c43a83225e7fe397800afddfe7165c6951
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-06 16:16:15 +0530

    connected to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
 
-dotenv.config();
+
+dotenv.config(); 
 
 const PORT = process.env.PORT || 5000;
+const URI = process.env.MONGODB_URI;   
+
+try {
+    mongoose.connect(URI);
+    console.log("connected to mongodb");
+    
+} catch (error) {
+    console.log(error);
+    
+}
 
 
 
