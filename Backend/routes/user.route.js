71a94888bf3f696b79155c1952ef34a154cc3807
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-08 18:17:16 +0530

    define route for login

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signup } from "../controllers/user.controller.js";
+import { login, signup } from "../controllers/user.controller.js";
 
 const router = express.Router();
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
