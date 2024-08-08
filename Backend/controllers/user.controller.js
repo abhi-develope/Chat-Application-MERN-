2bdb23c809a79abe7df97ccc1b21b090157d6a2f
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-08 18:15:39 +0530

    define login controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -26,6 +26,27 @@ export const signup = async (req, res) => {
        res.status(201).json({ message: "User created successfully", newUser });
    }
   } catch (error) {
-    console.log(error);
+    console.log({error: "internal server error"});
   }
 };
+
+export const login = async(req, res)=> {
+    try {
+        const {email, password} = req.body;
+        const user = User.findOne({email})
+        const isMatch = bcrypt.compare(password, user.password)
+        if(!user || !isMatch) {
+            return res.status(400).json({error: "invalid user credential"});
+        }
+        createTokenAndSaveCookie(user._id, res);
+        res.status(200).json({message: "User logged in successfully", user:{
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email
+        }});
+        
+    } catch (error) {
+        console.log({error: "internal server error"});
+        
+    }
+}
