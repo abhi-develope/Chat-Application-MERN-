75e3588e8cab401d6999efcd0faf8d740edca5a1
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-10 05:12:10 +0530

    logout route

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login, signup } from "../controllers/user.controller.js";
+import { login, logout, signup } from "../controllers/user.controller.js";
 
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
