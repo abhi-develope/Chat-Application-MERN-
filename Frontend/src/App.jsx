560de3391b135146c4919c225ac32ca82d56b904
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-10 16:52:36 +0530

    signup ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Profile from './home/leftside/Profilebox'
 import Chat from './home/rightside/Chatbox'
+import Signup from './components/Signup'
 
 function App() {
   return (
-    <div className='flex h-screen'> 
-      <Profile/>
-      <Chat/>
+    // <div className='flex h-screen'> 
+    //   <Profile/>
+    //   <Chat/>
+    // </div>
+    <div>
+      <Signup/>
     </div>
   )
 }
