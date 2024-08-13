c51baf3b18b8cd2e1e2e618b52c03dd874b11702
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-13 22:25:47 +0530

    add signup data to database

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form"
+import axios from "axios"
 
 function Signup() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+
+      //watch the password and confirm password fields
+      const password = watch("password", "");
+      const confirmPassword = watch("confirmPassword", "");
+
+      const validatePasswordMatch=(value)=>{
+        return value===password || "passwords do not match"
+      }
+
+
+      const onSubmit = (data) => {
+        const userInfo ={
+            fullname:data.fullname,
+            email:data.email,
+            password:data.password,
+            confirmPassword:data.confirmPassword,
+        }
+        // console.log(userInfo);
+        axios.post("http://localhost:3000/user/signup", userInfo).then((response) => {
+            console.log(response.data);
+            if(response.data){
+
+                alert("signup successful")
+            }
+           
+        }).catch((err) => {
+           console.log(err); 
+        });
+        
+      }
+    
   return (
     <div className="flex h-screen items-center justify-center">
-      <form action="" className="border border-white px-6 py-2 rounded-md space-y-3 w-96">
+      <form onSubmit={handleSubmit(onSubmit)} className="border border-white px-6 py-2 rounded-md space-y-3 w-96">
         <h1 className="text-2xl text-center">Chat <span className="text-[#00ffff] font-semibold">App</span></h1>
         <h2 className="text-white font-bold text-xl">Signup</h2>
         <br />
@@ -17,8 +56,9 @@ function Signup() {
           >
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
-          <input type="text" className="grow" placeholder="Username" />
+          <input type="text" className="grow" placeholder="fullname" {...register("fullname", { required: true })} />
         </label>
+        {errors.fullname && <span className="text-red-500 text-sm font-semibold">This field is required</span>}
         {/*Email  */}
         <label className="input input-bordered flex items-center gap-2">
   <svg
@@ -31,8 +71,9 @@ function Signup() {
     <path
       d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
   </svg>
-  <input type="text" className="grow" placeholder="Email" />
+  <input type="email" className="grow" placeholder="Email" {...register("email", { required: true })} />
 </label>
+{errors.email && <span className="text-red-500 text-sm font-semibold">This field is required</span>}
 {/* password */}
 <label className="input input-bordered flex items-center gap-2">
   <svg
@@ -45,8 +86,9 @@ function Signup() {
       d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
       clipRule="evenodd" />
   </svg>
-  <input type="password" className="grow" placeholder="password" />
+  <input type="password" className="grow" placeholder="password" {...register("password", { required: true })} />
 </label>
+{errors.password && <span className="text-red-500 text-sm font-semibold">This field is required</span>}
 {/* confirm password */}
 <label className="input input-bordered flex items-center gap-2">
   <svg
@@ -59,8 +101,9 @@ function Signup() {
       d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
       clipRule="evenodd" />
   </svg>
-  <input type="password" className="grow" placeholder="confirm password" />
+  <input type="password" className="grow" placeholder="confirm password" {...register("confirmPassword", { required: true, validate: validatePasswordMatch })} />
 </label>
+{errors.confirmPassword && <span className="text-red-500 text-sm font-semibold">{errors.confirmPassword.message}</span>}
 {/* text and button */}
 <div className="flex justify-between">
 <p>Have an account? <span className="text-blue-600 underline cursor-pointer ml-1">Login</span></p>
