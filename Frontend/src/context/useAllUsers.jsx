9fbb8f7e57f0ac134d85839b86eda0001b58d4ea
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-15 22:40:39 +0530

    create useAllUsers hook to fetch data from db in frontend

---
+++ Frontend/src/context/useAllUsers.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import Cookies from "js-cookie"
+import axios from "axios"
+
+function useAllUsers() {
+    const [allUsers, setAllUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        const getUsers = async()=>{
+            setLoading(true)
+            try {
+                const token = Cookies.get("jwt")
+                const response = await axios.get("http://localhost:3000/user/allusers", {
+                    credentials: "include",
+                    headers:{
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                setAllUsers(response.data)
+                setLoading(falsde)
+                
+            } catch (error) {
+               console.log("error in useAllUser:" + error); 
+            }
+        }
+        getUsers()
+    },[])
+
+  return [allUsers, loading]
+}
+
+export default useAllUsers
