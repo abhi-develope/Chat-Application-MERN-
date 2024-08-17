6ce9d40e5a98ca30d78305f269babdfd1b56ce63
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-17 17:39:42 +0530

    created message model

---
+++ Backend/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    receiverId: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    message:{
+        type: String,
+        required: true,
+    }
+},{timestamps: true});
+
+const Message = mongoose.model("message", messageSchema)
+
+export default Message;
