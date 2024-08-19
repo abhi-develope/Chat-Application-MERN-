2551832b2d35a84bd65b432498b1343391131c00
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-19 11:21:47 +0530

    define route for getting message from mongodb

--- Backend/routes/message.route.js
+++ Backend/routes/message.route.js
@@ -8,5 +8,6 @@ import secureRoute from "../middleware/secureRoute.js";
 
 const router = express.Router();
 router.post("/send/:id", secureRoute, sendMessage);
+router.post("/get/:id", secureRoute, getMessage);
 
 export default router;
