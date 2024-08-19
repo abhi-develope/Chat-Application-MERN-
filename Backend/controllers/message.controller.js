1a5f2684453c546de10500f0f09c4a88da5e12d9
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-19 16:43:06 +0530

    define controller to get message

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -36,3 +36,22 @@ export const sendMessage = async (req, res) => {
        res.status(500).json({error: "Internal server error"})
     }
 };
+
+export const getMessage = async (req, res) => {
+    try {
+        const {id: chatUser} = req.params;
+        const senderId = req.user._id;
+        let conversation = await Conversation.findOne({
+            members: {$all: [senderId, chatUser]},
+        }).populate("messages");
+        if(!conversation){
+            return res.status(201).json([])
+        }
+        const messages = conversation.messages;
+        res.status(201).json(messages);
+    } catch (error) {
+        console.log("Error in getMessage", error); 
+       res.status(500).json({error: "Internal server error"})
+        
+    }
+}
--- Backend/routes/message.route.js
+++ Backend/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessage, sendMessage } from "../controllers/message.controller.js";
 import secureRoute from "../middleware/secureRoute.js";
 
 
@@ -8,6 +8,6 @@ import secureRoute from "../middleware/secureRoute.js";
 
 const router = express.Router();
 router.post("/send/:id", secureRoute, sendMessage);
-router.post("/get/:id", secureRoute, getMessage);
+router.get("/get/:id", secureRoute, getMessage);
 
 export default router;
