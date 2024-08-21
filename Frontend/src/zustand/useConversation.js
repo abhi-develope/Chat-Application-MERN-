3610db55d76ee9360f8604d110f7e7f92feba84f
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-21 16:35:19 +0530

    create useConversation(global state)

---
+++ Frontend/src/zustand/useConversation.js
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+const useConversation = create((set) => ({
+ selectedConversation: null,
+  setSelectedConversation: (selectedConversation) => set({selectedConversation}),
+  messages: [],
+  setMessage: (messages) => set({ messages }),
+}));
