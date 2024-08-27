4cd4616ad3bba7f6ba7652878ca3f270a0819a5d
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-27 18:38:59 +0530

    take userId from socketio-client

--- Backend/socketIO/server.js
+++ Backend/socketIO/server.js
@@ -7,14 +7,21 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001/",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
 
+const users = {};
+
 // used to listen events on server side.
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    users[userId] = socket.id;
+    console.log("userId", users);
+  }
 
   // used to listen client side events emitted by server and client
   socket.on("disconnect", () => {
