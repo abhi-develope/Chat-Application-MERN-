8dd0d527ca2ce47a216bed6c8ce5a059ded635a3
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-28 16:25:43 +0530

    create useSocketContext hook to get the value of socket and online users

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -1,26 +1,41 @@
-import { createContext, useEffect, useState } from "react"
-import {useAuth} from "./Authprovider"
-import io from "socket.io-client"
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./Authprovider";
+import io from "socket.io-client";
 
 const socketContext = createContext();
 
-export const SocketProvider = ({children})=>{
-    const [socket, setSocket] = useState(null);
-    const [authUser] = useAuth();
-
-    useEffect(()=>{
-        if(authUser) {
-            const socket = io("http://localhost:3000", {
-                query: {
-                    userId: authUser.user._id,
-                },
-            });
-            setSocket(socket);
-        }
-    }, [authUser]);
-    return (
-        <socketContext.Provider value={{socket}}>
-            {children}
-        </socketContext.Provider>
-    )
+export const useSocketContext=()=>{
+    return useContext(socketContext)
 }
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [authUser] = useAuth();
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:3000", {
+        query: {
+          userId: authUser.user._id,
+        },
+      });
+      setSocket(socket);
+      socket.on("getOnlineUsers", (users)=>{
+        setOnlineUsers(users);
+      })
+      return()=>socket.close();
+    } else{
+        if(socket){
+            socket.close();
+            setSocket(null);
+        }
+
+    }
+  }, [authUser]);
+  return (
+    <socketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </socketContext.Provider>
+  );
+};
