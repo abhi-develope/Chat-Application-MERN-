5ed2a82f049f2d9baf0db0300ffab56c7f775e62
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-28 17:13:02 +0530

    implement the functionality of whatever a user is online or not

--- Frontend/src/home/leftside/User.jsx
+++ Frontend/src/home/leftside/User.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import useConversation from "../../zustand/useConversation.js";
+import {useSocketContext} from "../../context/SocketContext.jsx"
 
 function User({user}) {
   const {selectedConversation, setSelectedConversation} = useConversation();
   const isSelected = selectedConversation?._id === user._id;
+  const {socket, onlineUsers} = useSocketContext()
+  const isOnline = onlineUsers.includes(user._id)
   return (
     <div className={`hover:bg-slate-700 duration -300 ${isSelected ? "bg-slate-600": ""}`} onClick={()=> setSelectedConversation(user)}>
       <div className="flex space-x-4 p-4 cursor-pointer ">
-        <div className="avatar online ">
+      <div className={`avatar ${isOnline?"online": ""}`} >
           <div className="w-16 rounded-full">
             <img src="../img/dpimage.jpg" />
           </div>
