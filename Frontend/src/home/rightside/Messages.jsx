faf51723a9a6180094ba23f0d27008b58ecdcade
abhi-develope <prajapatiabhishek320@gmail.com>
2024-08-30 18:34:04 +0530

    add time to mesage

--- Frontend/src/home/rightside/Messages.jsx
+++ Frontend/src/home/rightside/Messages.jsx
@@ -6,16 +6,18 @@ const Messages = ({message}) => {
 
   const chatName = itsMe ? "chat-end" : "chat-start";
   const chatColor = itsMe ? "chat-bubble-success" : ""
+
+  const createdAt = new Date(message.createdAt)
+  const formattedTime = createdAt.toLocaleTimeString([],{
+    hour: '2-digit',
+    minute: '2-digit'
+  })
   return (
     <div>
-      {/* <div className={`chat ${chatName}`}>
-        <div className="chat-bubble">
-         hi Abhishek
-        </div>
-      </div> */}
       <div className={`chat ${chatName}`}>
         <div className={`chat-bubble text-white ${chatColor}`}> {message.message}
         </div>
+        <div className="chat-footer">{formattedTime}</div>
       </div>
     </div>
   );
